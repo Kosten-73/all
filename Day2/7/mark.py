96d9e49595d444c8e54ae3e619146b82e68a68ae
Konstantin Rudenko <korudenko@sfedu.ru>
2025-12-22 23:44:05 +0300

    марк

--- Day2/7/mark.py
+++ Day2/7/mark.py
@@ -1,10 +1,13 @@
-def t_sec(b, s, d, v):
-    return v / (s * b * d)
-
+# def t_sec(b, s, d, v):
+#     return v / (s * b * d)
 
 b = 64000
 s = 2
 d = 24
 v = 220 * 2**23
 
-print(int(t_sec(b, s, d, v) / 60))
+t = v / (s * b * d)
+
+print(int(t / 60))
+
+# print(int(t_sec(b, s, d, v) / 60))
